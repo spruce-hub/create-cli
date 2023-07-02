09d17819f85ae76fc69eed77dc0451c544473f1e
Noah <noah4882@163.com>
2023-07-03 01:20:31 +0800

    feat(template): update `@spruce-hub/nuxt-fetch` `@spruce-hub/nuxt-route`

--- template/nuxt/_nuxt.config.ts
+++ template/nuxt/_nuxt.config.ts
@@ -30,35 +30,34 @@ export default defineNuxtConfig({
     },
   },
   css: ['~/assets/scss/base.scss'],
-  <%_ if(module.filter((depsObj) => depsObj && depsObj.name && depsObj.name === '@element-plus/nuxt').length > 0) { _%>
-  modules: ['@spruce-hub/nuxt-route', '@spruce-hub/nuxt-fetch', '@element-plus/nuxt'],
-  elementPlus: {
-    importStyle: 'scss',
-  },
-  <%_ } else { _%>
-  modules: ['@spruce-hub/nuxt-route', '@spruce-hub/nuxt-fetch'],
-  <%_ } _%>
-  nuxtRoute: {
-    // 需要鉴权的路由
-    authPath: ['/presonal/*', '/product/**'],
-    // 登录页面的路由
-    loginPath: '/login',
-    // 记录 token 的 cookie name
-    tokenName: 'access_token',
-    // 登录成功之后不可重定向至以下路由，
-    excludePath: ['/login', '/register'],
-  },
-  runtimeConfig: {
-    nuxtFetch: {
-      // 需要代理的路由前缀
-      baseApi: '/api',
-      // 目标服务地址
-      apiHostUrl: 'https://api.online.dev.fyunshan.com',
-      apiHostEnv: 'API_HOST',
-      // 从 cookie 中获取名为 access_token 的值并写入请求头 Authorization 中
-      authorization: 'access_token',
-    },
-  },
+  modules: [
+    [
+      '@spruce-hub/nuxt-route',
+      {
+        authPath: ['/order', '/personal/'],
+        loginPath: '/login',
+        cookieName: 'access_token',
+        excludePath: ['/login', '/register'],
+      },
+    ],
+    [
+      '@spruce-hub/nuxt-fetch',
+      {
+        apiBase: '/api',
+        apiHostEnv: 'API_HOST',
+        apiHostUrl: 'https://api.fyunshan.com/',
+        cookieName: 'access_token',
+      },
+    ],
+    <%_ if(module.filter((depsObj) => depsObj && depsObj.name && depsObj.name === '@element-plus/nuxt').length > 0) { _%>
+    [
+      '@element-plus/nuxt',
+      {
+        importStyle: 'scss',
+      }
+    ]
+    <%_ } _%>
+  ],
   plugins: [],
   devServer: {
     port: 3000,
